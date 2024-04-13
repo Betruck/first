659ac0c29deb8392fe6c7bf229cb5006fc08738d
Betruck Solomon <134407709+Betruck@users.noreply.github.com>
2024-04-13 22:36:36 +0300

    Update hello.c

--- hello.c
+++ hello.c
@@ -17,9 +17,19 @@ void english(){
 }
 
 int main{
-	french();
-	german();
-	spanish();
-	english();
+	int num;
+	printf("Choose Language: 1. French /t 2. German /t 3. Spanish /t 4. English /n");
+	scanf("%d", num);
+	if(num == 1){
+		french();
+	} else if(num == 2){
+		german();
+	} else if(num == 3){
+		spanish();
+	} else if(num == 4){
+		english();
+	} else {
+		printf("Error!!!");
+	}
 	return 0;
 }
